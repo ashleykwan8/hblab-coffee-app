fb4d91ca9d5434eb5911af645663ccf2da2d5048
Ashley Kwan <ashleykwan888@gmail.com>
2020-10-21 22:43:00 +0000

    inital commit

---
+++ static/js/coffeeShop.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const addItemToCart = (itemName) => {
+  $('#cart-items').append(`
+    <tr>
+      <td>${itemName}</td>
+    </tr>
+  `);
+};
+
+const resetCart = () => {
+  $('#cart-total').html('0.00');
+  $('#cart-items').empty();
+};
+
+const incrementCartTotal = (price) => {
+  const cartTotal = $('#cart-total');
+
+  let total = Number(cartTotal.html());
+  total += price;
+
+  cartTotal.html(total.toFixed(2));
+};
+
+const incrementCoffeeSold = (amountSold) => {
+  let coffeeSold = Number($('#coffee-sold-counter').html());
+  coffeeSold += amountSold;
+
+  $('#coffee-sold-counter').html(coffeeSold);
+};
+
+const setProgressAndStatus = (progressVal, statusMsg) => {
+  $('#order-progress').attr('value', progressVal);
+  $('#order-status-message').html(statusMsg);
+};
+
+
+//
+// Add your event handlers below.
+//
